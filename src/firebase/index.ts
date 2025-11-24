d02737fb141fb079edb9ba2bb737e116bd4bba65
Ddm Martin <martinddm301@gmail.com>
2025-11-24 16:42:45 +0000

    // firebase.js import { initializeApp } from "firebase/app"; import { ge

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 // IMPORTANT: Do not modify this file. It is a an auto-generated file that is part of the
 // app's Firebase scaffolding. It is intended to be modified by the Studio AI agent.
 'use client';
@@ -9,17 +10,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // In a development environment, we always initialize with the config object.
-    // In a production environment (like Firebase App Hosting), initializeApp()
-    // can be called without arguments to automatically use the hosting environment's config.
-    // However, for simplicity and robustness across all environments, we will consistently use the local config.
-    const firebaseApp = initializeApp(firebaseConfig);
-    return getSdks(firebaseApp);
+  if (getApps().length) {
+    return getSdks(getApp());
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  const firebaseApp = initializeApp(firebaseConfig);
+  return getSdks(firebaseApp);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
